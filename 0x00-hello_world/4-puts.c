3c9fa302adfe59518f9c7d0f7dd5d4b5d8e98ea0
eyosiyas eyasu <deadlyhacker607@gmail.com>
2021-08-27 09:55:03 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)   // This is the return type (void means no value is computes)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);

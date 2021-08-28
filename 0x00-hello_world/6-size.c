ef68ead609eaa178de33150cf7d15d4ced88c7c9
eyosiyas eyasu <deadlyhacker607@gmail.com>
2021-08-28 06:48:46 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,16 @@
  */
 int main(void)
 {
-printf("Holberton School");
+  char charType;
+  int intType;
+  long int longintType;
+  long long int longlongintType;
+  float floatType;
+  
+  printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+  printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+  printf("Size of a long int: %zu byte(s)\n", sizeof(intType));
+  printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+  printf("size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }

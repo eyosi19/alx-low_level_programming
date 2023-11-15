ea68e51df2d8a5f37d55e51a3169f19463036156
Eyosiyas <eyasueyosiyas@gmail.com>
2023-11-15 13:39:15 -0800

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,15 +11,12 @@
 
 void _put_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		s++;
-		_put_recursion(s);
-	}
-	else
+	if (*s == "\0")
 	{
 		_putchar("\n");
 		return;
 	}
+	_putchar(*s);
+	s++;
+	_put_recursion(s);
 }

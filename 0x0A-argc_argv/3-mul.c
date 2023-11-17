21c88632e4411115efb0a8d9b44e0f835e9487cc
Eyosiyas <eyasueyosiyas@gmail.com>
2023-11-17 13:43:48 -0800

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,14 +14,16 @@
 
 int main(int argc, char *argv[])
 {
+	int x, y, sum;
+
 	if (argc != 3)
 	{
 		printf("Error.\n");
 		return (1);
 	}
 
-	int x = atoi(argv[1]);
-	int y = atoi(argv[2]);
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
 
 	int sum = x * y;
 

d2f5561d503cefb6fb670c0f1962691166c03cfc
Eyosiyas <eyasueyosiyas@gmail.com>
2023-11-17 13:19:09 -0800

    argv argc

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,9 +11,9 @@
  * Return: 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	for (int i = 0; argv[i] != NULL; i++)
+	for (int i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}

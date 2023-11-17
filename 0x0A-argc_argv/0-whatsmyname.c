7e029f085aea30c27aaad62cf334b2e2427ac2bc
Eyosiyas <eyasueyosiyas@gmail.com>
2023-11-17 12:11:01 -0800

    argv, argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - returns the arguments
+ *
+ * @argc: counts the arguments
+ *
+ * @argv: the value of the arguments
+ *
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);

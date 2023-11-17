f69979ea293c3dc7d9ea4444f9c322684f1105ae
Eyosiyas <eyasueyosiyas@gmail.com>
2023-11-17 14:08:27 -0800

    argc argv atoi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,38 +2,6 @@
 #include "main.h"
 
 
-int atoi(char *s);
-
-/**
- * main - returns the result of two arguments multiply
- *
- * @argc: number of arguments
- *
- * @argv: arrays of arugments
- *
- * Return: 0
- */
-
-int main(int argc, char *argv[])
-{
-	int x, y, sum;
-
-	if (argc < 3 || argc > 3)
-	{
-		printf("Error.\n");
-		return (1);
-	}
-
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-
-	sum = x * y;
-
-	printf("%d\n", sum);
-
-	return (0);
-}
-
 /**
  * atoi - change char to integer
  *
@@ -61,4 +29,33 @@ int atoi(char *s)
 		s++;
 	}
 	return (sign * result);
+}
+
+/**
+ * main - returns the result of two arguments multiply
+ *
+ * @argc: number of arguments
+ *
+ * @argv: arrays of arugments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y, sum;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error.\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+
+	sum = x * y;
+
+	printf("%d\n", sum);
+	return (0);
 }

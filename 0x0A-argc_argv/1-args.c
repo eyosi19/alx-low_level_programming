c536e930eb57eeae9e77741d1dd74876d4a44d2d
Eyosiyas <eyasueyosiyas@gmail.com>
2023-11-17 12:46:48 -0800

    argv, arc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - returns the number of the arguments
+ *
+ * @argc: number of arguments
+ *
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

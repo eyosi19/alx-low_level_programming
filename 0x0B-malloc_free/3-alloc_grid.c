91c4be801c26a61b58857155dc5e361bff5de0bf
Eyosiyas <eyasueyosiyas@gmail.com>
2023-11-21 13:16:33 -0800

    alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,19 +13,31 @@
 
 int **alloc_grid(int width, int height)
 {
-	int *grid;
+	int **grid;
+	int i;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	grid = malloc(width * height * sizeof(int));
+	grid = malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+
+		if (grid[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+
 
 	return (grid);
 }

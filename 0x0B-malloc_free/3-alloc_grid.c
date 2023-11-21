5b9f0675420efdb6ee5621532f8beeeea403410b
Eyosiyas <eyasueyosiyas@gmail.com>
2023-11-21 13:04:43 -0800

    alloc_grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocate a grid
+ *
+ * @width: the columns for the grid
+ *
+ * @height: the row for the grid
+ *
+ * Return: return the allocated
+ */
+
+int **alloc_grid(int width, int height)
+{
+	char *grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = malloc(width * height * sizeof(int));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	return (grid);
+}
+
+

a99159498e9a67f0601b88524f4b98d35daec896
Eyosiyas <eyasueyosiyas@gmail.com>
2023-11-23 11:52:05 -0800

    callloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - calloc
+ *
+ * @nmemb: the array
+ *
+ * @size: the size
+ *
+ * Return: hte array of a given size
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	int *s;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	s = malloc(nmemb * size * sizeof(int));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+
+	for (i = 0; i < size * nmemb; i++)
+	{
+		s[i] = 0;
+	}
+	return (s);
+}

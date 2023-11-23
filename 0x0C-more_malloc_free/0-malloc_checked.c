6d070bf020cc34c403e11da212e55a483aeb8189
Eyosiyas <eyasueyosiyas@gmail.com>
2023-11-23 10:46:32 -0800

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - allocate memory
+ *
+ * @b: this the integer
+ *
+ * Return: 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *s;
+
+	s = malloc(b * sizeof(int));
+
+	if (s == NULL)
+	{
+		return (98);
+	}
+}

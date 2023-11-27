a2140e16c322d57153f8138bf6d8ff5b8128a8a5
Eyosiyas <xoyosi19@gmail.com>
2023-11-27 18:09:17 -0500

    new_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+/**
+ * new_dog - the new dog
+ *
+ * @name: the name of the dog struct
+ *
+ * @age: the age of the dog struct
+ *
+ * @owner: the owner
+ *
+ * Return: newDog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = malloc(sizeof(struct dog));
+
+	if (newDog != NULL)
+	{
+		newDog->name = strdup(name);
+		newDog->age = age;
+		newDog->owner = strdup(owner);
+
+		if (newDog->name == NULL || newDog->owner == NULL)
+		{
+			free(newDog->name);
+			free(newDog->owner);
+			free(newDog);
+			return (NULL);
+		}
+	}
+
+	return (newDog);
+}
+

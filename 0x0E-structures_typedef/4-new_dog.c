75a2307c33d1cf74ee8611d06ffea419b9dab35b
Eyosiyas <xoyosi19@gmail.com>
2023-11-27 18:30:33 -0500

    free_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,46 @@
 #include <stdlib.h>
 #include "dog.h"
 
+
+/**
+ * _strlen - returns length of string
+ *
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * _strcpy - copy string
+ *
+ * @dest: destination
+ *
+ * @src: source
+ *
+ * Return: pointer
+ */
+
+char *_strcpy(char *dest, *char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+}
+
+
 /**
  * new_dog - the new dog
  *
@@ -18,7 +58,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog;
 
-	newDog = malloc(sizeof(struct dog));
+	newDog = malloc(sizeof(dog_t));
 
 	if (newDog != NULL)
 	{

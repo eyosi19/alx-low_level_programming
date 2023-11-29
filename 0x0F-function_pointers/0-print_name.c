953e9c1ed534864432773e36bcfd1344748ed677
Eyosiyas <xoyosi19@gmail.com>
2023-11-29 12:57:12 -0500

    function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,28 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
+/**
+ * print_normal - will print normally
+ *
+ * @name: the argument to print
+ *
+ * Return: 0;
+ */
 void print_normal(char *name)
 {
 	printf("%s\n", name);
 }
+
+/**
+ * print_name - will call back the function
+ *
+ * @name: the argument (the name)
+ *
+ * @f: the pointer to function
+ *
+ * Return: 0;
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);

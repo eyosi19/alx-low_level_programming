575416245ce83e0c6f63ae0ad3026769de9736f6
Eyosiyas <xoyosi19@gmail.com>
2023-12-08 16:07:02 -0500

    TEAMWORK

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_list - prints all in the list
+ *
+ * @h: the pointer to the list_t
+ *
+ * Return: the number of the node
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (*h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef __LISTS_H__
+#define __LISTS_H__
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - sinlgy linked list
+ *
+ * @str: string of malloc
+ *
+ * @len: the len of the string
+ *
+ * @next: points to the next node
+ *
+ * Description: singly linked list node struct
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
+size_t print_list(const list_t *h);
+
+#endif

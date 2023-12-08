06e20433676a04efce834a7d5d44597a75c4919e
Eyosiyas <xoyosi19@gmail.com>
2023-12-08 16:26:38 -0500

    single_linked_lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,6 @@
-#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
  * list_len - list the length of the single link list
@@ -12,13 +12,13 @@
 
 size_t list_len(const list_h *h)
 {
-	size_t n = 0;
+	size_t c = 0;
 
 	while (h)
 	{
 		h = h->next;
-		n++;
+		c++;
 	}
 
-	return (n);
+	return (c);
 }

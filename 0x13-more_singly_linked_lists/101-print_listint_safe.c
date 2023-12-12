69ad0a146989b16b4350ad27ff6845637947d85a
Eyosiyas <xoyosi19@gmail.com>
2023-12-12 14:52:51 -0500

    single_linked_lists

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t liked list safetly
+ *
+ * @head: the pointer to the head
+ *
+ * Return: the number of the node
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	size_t count = 0;
+
+	slow = head;
+	fast = head;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+
+		slow = slow->next;
+		fast = fast->next->next;
+
+		count++;
+
+		if (slow == fast)
+		{
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
+			exit(98);
+		}
+	}
+
+	return (count);
+}

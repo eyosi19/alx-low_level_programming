81fae8f4ed1adc6afe6c2f68b9d869e4e0acc943
Eyosiyas <xoyosi19@gmail.com>
2023-12-12 14:01:59 -0500

    single_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -35,7 +35,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = current->next;
 	}
 
-	if (current == NULL || current->next == NULL)
+	if (current == NULL)
 	{
 		return (-1);
 	}

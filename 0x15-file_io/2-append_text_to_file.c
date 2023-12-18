4f30e1ed0794015ea9b2e19919cfebb1ed4bfcb2
Eyosiyas <xoyosi19@gmail.com>
2023-12-18 11:09:00 -0500

    file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,7 +32,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	ssize_t w = write(fd, text_content, strlen(text_content));
+	w = write(fd, text_content, strlen(text_content));
 
 	if (w == -1)
 	{

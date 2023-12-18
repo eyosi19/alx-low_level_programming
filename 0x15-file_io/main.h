a29adda10af69b7d7203ad0d17a8087a27abb2f3
Eyosiyas <xoyosi19@gmail.com>
2023-12-18 11:50:28 -0500

    file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,13 @@
 #define MAIN_H
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <string.h>
+
+#define BUFFER_SIZE 1024
 
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);

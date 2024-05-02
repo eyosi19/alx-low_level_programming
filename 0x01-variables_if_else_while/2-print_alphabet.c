d3a260dead5d8fbc07397e06545be35ccd71ee8d
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-02 11:35:32 -0400

    if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the main funciton
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c<= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

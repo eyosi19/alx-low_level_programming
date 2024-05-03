0e3979e2799d442862346a5b1093f7643badfa15
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-03 12:50:11 -0400

    functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 		for (m = 0; m < 60; m++)
 		{
 			_putchar(h / 10 + '0');
-			_putcahr(h % 0 + '0');
+			_putchar(h % 10 + '0');
 			_putchar(':');
 			_putchar(m / 10 + '0');
 			_putchar(m % 10 + '0');

2ba5f8de2e7f711a41bc2437c08d3f066596f25a
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-03 13:08:00 -0400

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - print from n to 98
+ *
+ * @n: number
+ *
+ * Return: the number form n 98
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+	}
+}

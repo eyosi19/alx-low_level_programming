cf0c18d71a95ccfa6bd4cab0f59d3d9511d5a2c0
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-03 11:11:27 -0400

    functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,16 +11,15 @@ void print_alphabet_x10(void)
 	char c;
 	int i;
 
-	c = 'a';
 	i  = 0;
 
 	while (i < 10)
 	{
+		c = 'a';
 		while (c <= 'z')
 		{
 			putchar(c);
 			c++;
 		}
-		putchar('\n');
 	}
 }

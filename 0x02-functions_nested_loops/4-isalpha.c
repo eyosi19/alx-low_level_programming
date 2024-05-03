05b69c08743fec402dda9b1e1c6ea8de5bf7d1ba
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-03 11:51:32 -0400

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _isalpha - return 1 if c is a letter, upper or lower case.
+ *
+ * @c: the character
+ *
+ * Return: 1 if it is letter 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}

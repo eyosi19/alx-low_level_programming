3a14d1f741b02779bca54cdb0b3a6afa7cf9998f
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-03 10:55:08 -0400

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	c = 'a';
+	i  = 0;
+
+	while (i < 10)
+	{
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+	}
+}

8ca1779d104344d9e15167452f0c45c4077d4bcb
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-07 15:41:23 -0400

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - print the string in rev
+ *
+ * @s: the string
+ */
+
+void print_rev(char *s)
+{
+	int i, len;
+	char *rev;
+
+	len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

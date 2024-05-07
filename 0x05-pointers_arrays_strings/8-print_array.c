6ad9a87a128b6c53f05488d1b8e5496ac18e7092
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-07 18:39:14 -0400

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print the n number of arrays
+ *
+ * @a: the array
+ *
+ * @n: the n number
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d, ", *a);
+	}
+}

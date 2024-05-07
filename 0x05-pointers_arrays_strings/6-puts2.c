b74164a8291a6fc7efb3c21550efb1df068c0c0f
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-07 18:14:06 -0400

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ *
+ * @str: the string
+ */
+
+void puts(char *str)
+{
+	int i, len;
+
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	_putchar(str[0]);
+
+	for (i = (len - 1); i >= 0; i--)
+	{
+		_putchar(str[i + 1]);
+	}
+	_putchar('\n');
+}

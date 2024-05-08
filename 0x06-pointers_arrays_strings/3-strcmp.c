e843bede78f023f29624e1b44eff020d67472b37
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-08 16:36:25 -0400

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,23 +12,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, len1, len2;
+	int i;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		len1++;
-	for (j = 0l s2[j] != '\0'; j++)
-		len2++;
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
 
-	if (len1 == len2)
-	{
-		return (0);
-	}
-	else if (len1 > len2)
-	{
-		return (15);
-	}
-	else
-	{
-		return (-15);
-	}
+	return (s1[i] - s2[i]);
 }

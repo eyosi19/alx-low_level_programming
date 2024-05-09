41d6b8d02dd837c06616bf31e5b128b310821124
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-09 16:30:56 -0400

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,36 +2,38 @@
 /**
  * cap_string - capitalizes all the word
  *
- * @str: the string
+ * @s: the string
  *
  * Return: the cap_string
  */
 
 char *cap_string(char *s)
 {
+	int i, j;
+	char sepr[];
+
+	i = 0;
+	sepr[] = " ,;.!?\"()\t{}\n";
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+
+	while (s[i])
 	{
-    int i = 0, j;
-    char sepr[] = " ,;.!?\"()\t{}\n";
+		j = 0;
 
-    if (s[0] >= 'a' && s[0] <= 'z')
-        s[0] -= 32;
+		while (sepr[j])
+		{
+			if (s[i] == sepr[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
 
-    while (s[i])
-    {
-        j = 0;
-
-        while (sepr[j])
-        {
-            if (s[i] == sepr[j])
-            {
-                if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-                    s[i + 1] -= 32;
-                break;
-            }
-            j++;
-        }
-        i++;
-    }
-    return (s);
-}
 }

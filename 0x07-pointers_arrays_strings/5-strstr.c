8f45047c00406829a58b20e354c151e2e856aeab
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-11 18:24:45 -0400

    pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - located a substring
+ *
+ * @haystack: the string
+ *
+ * @needle: the substring
+ *
+ * Return: pointer to the beginning of the located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[i])
+		{
+			return (needle);
+		}
+	}
+	return (NULL);
+}

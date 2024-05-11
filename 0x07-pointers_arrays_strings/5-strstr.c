93fc9c5458a09b20b199b3e375f4db645cc070a1
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-11 18:27:38 -0400

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (haystack[i] == needle[i])
+		if (haystack[i] == *needle)
 		{
 			return (needle);
 		}

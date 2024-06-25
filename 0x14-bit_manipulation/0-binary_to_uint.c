ad35d1db408a287faece1bd4d3092f45195479a9
Eyosiyas <xoyosi19@gmail.com>
2024-06-25 06:57:05 -0700

    bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,28 +14,21 @@ unsigned int binary_to_uint(const char *b)
 
 	if (*b == 0)
 	{
-		printf("0");
+		return (0);
 	}
 
-	if (*b == '0' || *b == '1')
+	while (*b)
 	{
-		while (*b)
+		if (*b != '0' && *b != '1')
 		{
-			if (*b == '1')
-			{
-				result = result << 1;
-				result += 1;
-			}
-			if (*b == '0')
-			{
-				result = result << 1;
-			}
-			b++;
+			return (0);
 		}
-	}
-	else
-	{
-		printf("0");
+		result = result << 1;
+		if (*b == '1')
+		{
+			result += 1;
+		}
+		b++;
 	}
 	return (result);
 }

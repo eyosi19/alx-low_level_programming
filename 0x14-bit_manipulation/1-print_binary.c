90701e6ddb413f1b3a5be64a4c2147ab5e5f85a3
Eyosiyas <xoyosi19@gmail.com>
2024-06-25 07:50:54 -0700

    bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,17 +9,21 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, len = sizeof(unsigned long int) * 8;
+	int i, len = sizeof(unsigned long int) * 8, found = 0;
 
 	for (i = len - 1; i >= 0; i--)
 	{
 		if (n & (1UL << i))
 		{
+			found = 1;
 			printf("1");
 		}
-		else
+		else if (found)
 		{
 			printf("0");
 		}
 	}
+
+	if(!found)
+		printf("0");
 }

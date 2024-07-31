cf23658eac0b03a9dbd45b71f966c21f5fc19d8d
Eyosiyas <xoyosi19@gmail.com>
2024-07-31 13:12:56 -0700

    file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,16 +13,27 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t written;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
 
-	write(fd, text_content, strlen(text_content));
+	if (text_content != NULL)
+	{
+		written = write(fd, text_content, strlen(text_content));
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
 
 	return (1);
 }

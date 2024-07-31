b2e3156ebc4e43354faa9a909b52191d1534fef1
Eyosiyas <xoyosi19@gmail.com>
2024-07-31 12:53:19 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,20 +11,30 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t len, written;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
-		return (0);
+		return (-1);
 	}
 
-	write(fd, text_content, sizeof(text_content));
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		written = write(fd, text_content, len);
+		if (written == -1 || written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
 	close(fd);
 

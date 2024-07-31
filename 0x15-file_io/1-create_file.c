61d4f55067d078dcd931c41a9dafd5729ee46d1c
Eyosiyas <xoyosi19@gmail.com>
2024-07-31 12:43:53 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  * @text_content: is a NULL terminated string to write to the file
  */
 
-int create_file(const cahr *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
